555c9b96b8d1a9ed19fddee1074733ccc086e33b
S7440 <skylar.fynboh@outlook.com>
2023-01-08 16:13:15 -0600

    Refined plan 0.0.1

---
+++ index.js
@@ -0,0 +1,13 @@
+//cli code to create a web page for staff records.
+
+//start interface
+//Would youu like to create a team?
+    //if yes then run manager.js
+        //'add special employee field questions to manager.js'
+    //would you like to add a team member
+        //if yes run employee
+            //loop until answer is no
+            //employee questions run
+            //then choose intern or engineer
+            //++, Return
+        //else end program and create html
