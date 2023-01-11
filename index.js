999af1ec00f00e30e673ed7cbd1bac389a8b8f71
S7440 <skylar.fynboh@outlook.com>
2023-01-11 15:12:21 -0600

    Restarting computer commit

--- index.js
+++ index.js
@@ -2,12 +2,21 @@
 
 //start interface
 //Would youu like to create a team?
-    //if yes then run manager.js
-        //'add special employee field questions to manager.js'
+    //if yes
+        //run employee.js
+        //run manager.js
+    //if no return to start
     //would you like to add a team member
-        //if yes run employee
-            //loop until answer is no
-            //employee questions run
-            //then choose intern or engineer
-            //++, Return
+        //if yes
+            //run employee
+            //ask intern/engineer/cancel
+                //if intern
+                    //run intern.js
+                    //return to add a team member
+                //if manager
+                    //run manager.js
+                    //return to add a team member
+                //if cancel 
+                    //delete current card
+                    //return to add a team member
         //else end program and create html
